9cd9680ee620f67db3b4127540f08c5f7aab917a
jaco <jacoluna01@gmail.com>
2024-10-31 15:08:37 -0300

    add: floydWarshall algorith applied

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
+import Services.PathFindingService;
+
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
+        int[][] graph = {
+                {0, 4, Global.INF, 5, Global.INF},
+                {Global.INF, 0, 1, Global.INF, 6},
+                {2, Global.INF, 0, 3, Global.INF},
+                {Global.INF, Global.INF, 1, 0, 2},
+                {1, Global.INF, Global.INF, 4, 2}
+        };
+
+        HashMap<Character, HashMap<Character, Integer>> distances = PathFindingService.shortestPath(graph, Global.INF);
+
+        distances.forEach((key, value) -> {
+            System.out.println(key);
+            value.forEach((l, d) -> System.out.println(l + " " + d));
+        });
     }
 }
