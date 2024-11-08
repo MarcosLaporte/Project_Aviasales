b1be67f0a3495a804fa206b4e11132be301f503c
LapMarcos <78131374+MarcosLaporte@users.noreply.github.com>
2024-11-07 21:02:02 -0300

    UPDATE: Some refactor of methods and clean-up

--- src/main/java/services/RouteService.java
+++ src/main/java/services/RouteService.java
@@ -11,10 +11,9 @@ import java.util.Map;
 import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 
-
 public abstract class RouteService {
 
-    public static final int INF = Integer.MAX_VALUE / 2;
+    public static final Double INF = Double.POSITIVE_INFINITY;
 
     /**
      * Creates the graph with every row and column as a destination, having te direct distance between them as
@@ -25,34 +24,31 @@ public abstract class RouteService {
      * @param routes   List of routes connecting airports with distances.
      * @return A matrix representing the distances between each airport pair.
      */
-    private static double[][] makeKmGraph(List<Airport> airports, List<Route> routes) {
-        return makeGraph(airports, routes, Route::getKm);
-    }
-
-    private static double[][] makePriceGraph(List<Airport> airports, List<Route> routes) {
-        return makeGraph(airports, routes, Route::getPrice);
-    }
-
     private static double[][] makeGraph(List<Airport> airports, List<Route> routes, ToDoubleFunction<Route> function) {
-        double[][] graph = new double[airports.size()][airports.size()];
+        final int size = airports.size();
+        double[][] graph = new double[size][size];
 
-        for (int i = 0; i < airports.size(); i++) {
+        Map<String, List<Route>> routeMap = routes.stream()
+                .collect(Collectors.groupingBy(r -> r.getIdFrom() + "-" + r.getIdTo()));
+
+        for (int i = 0; i < size; i++) {
             Airport airportFrom = airports.get(i);
 
-            for (int j = 0; j < airports.size(); j++) {
-                Airport airportTo = airports.get(j);
-
+            for (int j = 0; j < size; j++) {
                 if (i == j) {
                     graph[i][j] = 0;
+                    continue;
+                }
+
+                Airport airportTo = airports.get(j);
+
+                List<Route> possibleRoutes = routeMap.get(airportFrom.getId() + "-" + airportTo.getId());
+                if (possibleRoutes != null) {
+                    graph[i][j] = possibleRoutes.stream()
+                            .mapToDouble(function).min()
+                            .orElse(INF);
                 } else {
-                    for (Route r : routes) {
-                        if (r.getIdFrom() == airportFrom.getId() && r.getIdTo() == airportTo.getId()) {
-                            graph[i][j] = function.applyAsDouble(r);
-                            break;
-                        } else {
-                            graph[i][j] = INF;
-                        }
-                    }
+                    graph[i][j] = INF;
                 }
             }
         }
@@ -64,17 +60,16 @@ public abstract class RouteService {
      * Applies the Floyd-Warshall algorithm to compute the shortest paths and track each path.
      * Updates the graph with the shortest distances and the paths list with corresponding paths.
      *
-     * @param graph  The distance matrix between airports.
-     * @param parent The parent matrix used to track paths.
-     * @param paths  A 3D list storing the paths for each airport pair.
+     * @param graph The distance matrix between airports.
+     * @param paths A 3D list storing the paths for each airport pair.
      */
-    private static void applyFloydWarshall(double[][] graph, int[][] parent, List<List<List<Integer>>> paths) {
-        int V = graph.length;
+    private static void applyFloydWarshall(double[][] graph, List<List<List<Integer>>> paths) {
+        initializeParentArray(graph, paths);
+        final int V = graph.length;
         for (int i = 0; i < V; i++) {
             for (int j = 0; j < V; j++) {
                 for (int k = 0; k < V; k++) {
                     if (graph[j][i] + graph[i][k] < graph[j][k]) {
-                        parent[j][k] = parent[i][k];
                         graph[j][k] = graph[j][i] + graph[i][k];
 
                         List<Integer> newPath = new ArrayList<>(paths.get(j).get(i));
@@ -94,18 +89,12 @@ public abstract class RouteService {
      *
      * @param graph The distance matrix for airport routes.
      * @param paths A 3D list to hold each path from source to destination.
-     * @return An initialized parent matrix to be used in path updates.
      */
-    private static int[][] initializeParentArray(double[][] graph, List<List<List<Integer>>> paths) {
-        int V = graph.length;
-        int[][] parent = new int[V][V];
-        // Initialize parent array
+    private static void initializeParentArray(double[][] graph, List<List<List<Integer>>> paths) {
+        final int V = graph.length;
         for (int i = 0; i < V; i++) {
-            parent[i][i] = i;
             paths.add(new ArrayList<>());
             for (int j = 0; j < V; j++) {
-                parent[i][j] = (i == j) ? i : (graph[i][j] != INF ? i : -1);
-
                 List<Integer> path = new ArrayList<>();
                 if (graph[i][j] != INF) {
                     path.add(i);
@@ -114,71 +103,69 @@ public abstract class RouteService {
                 paths.get(i).add(path);
             }
         }
-        return parent;
     }
 
     private static List<Route> getRoutes() {
         try (MyBatis<Route> routesDAO = new MyBatis<>(Route.class)) {
             return routesDAO.get(Map.of());
         } catch (Exception e) {
-            LoggerService.log(Level.ERROR, e.getMessage() != null ? e.getMessage() : e.getClass().toString());
+            return List.of();
         }
-
-        return List.of();
     }
 
     /**
-     * This should print the nearest route between airports
+     * Prints the shortest and cheapest routes between two airports using pre-calculated distances and prices.
+     * Calculates and displays the path, total distance, and price if routes exist between the airports.
      *
-     * @param startIndex start of the trip
-     * @param endIndex   destination of the trip
-     * @param airports   airport list
+     * @param startIndex The index of the departure airport in the list.
+     * @param endIndex   The index of the destination airport in the list.
+     * @param airports   The list of available airports, each represented by an Airport object.
      */
     public static void printRouteBetweenAirports(int startIndex, int endIndex, List<Airport> airports) {
+        if (startIndex == endIndex) {
+            LoggerService.consoleLog(Level.WARN, "Same airport selected as departure and destination.");
+            return;
+        }
+
         List<Route> routes = getRoutes();
         if (routes.isEmpty()) {
             LoggerService.log(Level.ERROR, "No routes found in database.");
+            return;
         }
 
-        // distance graph
-        double[][] kmGraph = makeKmGraph(airports, routes);
-        int[][] kmParent;
+        // Distance graph
+        double[][] kmGraph = makeGraph(airports, routes, Route::getKm);
         List<List<List<Integer>>> kmPaths = new ArrayList<>();
+        applyFloydWarshall(kmGraph, kmPaths);
 
-        kmParent = initializeParentArray(kmGraph, kmPaths);
-        applyFloydWarshall(kmGraph, kmParent, kmPaths);
-
-        // price graph
-        double[][] priceGraph = makePriceGraph(airports, routes);
-        int[][] priceParent;
+        // Price graph
+        double[][] priceGraph = makeGraph(airports, routes, Route::getPrice);
         List<List<List<Integer>>> pricePaths = new ArrayList<>();
+        applyFloydWarshall(priceGraph, pricePaths);
 
-        priceParent = initializeParentArray(priceGraph, pricePaths);
-        applyFloydWarshall(priceGraph, priceParent, pricePaths);
-
-        // shortest route
-        if (startIndex != endIndex && kmGraph[startIndex][endIndex] < INF) {
-            System.out.println("Shortest path from " + airports.get(startIndex).getName() +
-                    " to " + airports.get(endIndex).getName() + ":");
-            System.out.println(kmPaths.get(startIndex).get(endIndex)
-                    .stream().map(index -> airports.get(index).getName())
-                    .collect(Collectors.joining(" -> ")));
-            System.out.println(" (Distance: " + kmGraph[startIndex][endIndex] + " km)");
-        } else {
-            System.out.println("No available path between the selected airports by distance.");
+        if (kmGraph[startIndex][endIndex] == INF || priceGraph[startIndex][endIndex] == INF) {
+            LoggerService.consoleLog(Level.INFO, "No available path between the selected airports.");
+            return;
         }
+
+        // Shortest route
+        System.out.printf("Shortest route from %s to %s:\n", airports.get(startIndex).getName(), airports.get(endIndex).getName());
+
+        List<Integer> shortestPathAirlineIndex = kmPaths.get(startIndex).get(endIndex);
+        System.out.println(shortestPathAirlineIndex.stream()
+                .map(index -> airports.get(index).getName() + " (" + kmGraph[startIndex][index] + " km)")
+                .collect(Collectors.joining(" -> "))
+                + '\n');
+
 
         // Cheapest route
-        if (startIndex != endIndex && priceGraph[startIndex][endIndex] < INF) {
-            System.out.println("Cheapest path from " + airports.get(startIndex).getName() +
-                    " to " + airports.get(endIndex).getName() + ":");
-            System.out.println(pricePaths.get(startIndex).get(endIndex)
-                    .stream().map(index -> airports.get(index).getName())
-                    .collect(Collectors.joining(" -> ")));
-            System.out.println(" (Price: $" + priceGraph[startIndex][endIndex] + ")");
-        } else {
-            System.out.println("No available path between the selected airports by price.");
-        }
+        System.out.printf("Cheapest route from %s to %s:\n", airports.get(startIndex).getName(), airports.get(endIndex).getName());
+
+        List<Integer> cheapestPathAirlineIndex = pricePaths.get(startIndex).get(endIndex);
+        System.out.println(cheapestPathAirlineIndex.stream()
+                .map(index -> airports.get(index).getName() + " ($" + priceGraph[startIndex][index] + ")")
+                .collect(Collectors.joining(" -> "))
+                + '\n');
     }
 
 }
